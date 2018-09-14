91115632b724719a09fefdf1e62308be48751406
Juliano Custodio <juliano.custodio@hotmail.com.br>
2018-09-13 23:53:36 -0300

    Implement ListView with Infinite Scroll

---
+++ InfiniteScrollDemo/InfiniteScrollDemo/InfiniteScrollDemo/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xamarin.Forms.Extended;
+
+namespace InfiniteScrollDemo
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private const int PageSize = 8;
+        private bool _isBusy;
+        readonly Service _service = new Service();
+        public InfiniteScrollCollection<string> Items { get; }
+
+        public MainViewModel()
+        {
+            Items = new InfiniteScrollCollection<string>
+            {
+                OnLoadMore = async () =>
+                {
+                    IsBusy = true;
+
+                    // Ler a proxima pagina
+                    var page = Items.Count / PageSize;
+
+                    //Busca os itens
+                    var items = await _service.GetPessoasAsync(page, PageSize);
+
+                    IsBusy = false;
+
+                    // Itens que serão adicionados
+                    return items;
+                }
+            };
+
+            Download();
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private async Task Download()
+        {
+            var items = await _service.GetPessoasAsync(pageIndex: 0, pageSize: PageSize);
+
+            Items.AddRange(items);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        
+    }
+}
---
+++ InfiniteScrollDemo/InfiniteScrollDemo/InfiniteScrollDemo/Service.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfiniteScrollDemo
+{
+    public class Service
+    {
+
+        private readonly List<string> _pessoas = new List<string>
+        {
+            "Adriano", "José", "Maria","Oscar", "João", "Eduarda", "Daniel",
+            "Suzana", "Paulo", "Pedro","Davi", "Felipe", "Fernanda", "Regina", "Nicole",
+            "Jesus", "Samira", "Samuel", "Elis", "Mayara", "Leticia", "Luiz", "Luciano",
+            "Neto", "Rodrigo", "Silvia", "Joel", "Vitória", "Fernando", "Samanta", "Karen",
+            "Marcio", "Cleusa", "Clara", "Luciana", "Cleber", "Jairo", "Jair", "Douglas", "Hamilton",
+        };
+
+        public async Task<List<string>> GetPessoasAsync(int pageIndex, int pageSize)
+        {
+            //Delay para simular o tempo de uma requisição
+            await Task.Delay(3000);
+            return _pessoas.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+        }
+    }
+}
